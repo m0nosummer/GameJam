17e6826ab74322e0d2f502e502313160f4bfbd8a
m0no_summer <tomorrowisare@gmail.com>
2024-05-11 00:33:05 +0900

    update

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Player Data")]
+public class PlayerData : ScriptableObject
+{
+    [Header("Player Ability")] 
+    public float oxygenLeft;
+    public float 
+}
